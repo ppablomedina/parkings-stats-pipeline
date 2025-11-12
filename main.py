d47998dd295b6579078444feb2b8725e3fbc9945
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-11-12 17:50:32 +0000

    Refactor main.py to use Flask for entry point

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import processes.ocupacion, processes.abonados_en_banco, processes.abonados_lpa_y_qr, processes.recaudacion, processes.rincon_estadisticas, processes.abonados_y_rotacion, processes.informes_filtrados, processes.ratios
 from gcp.utils import insert_events
+from flask import Flask, request
 
 
 def entry_point(req):
     modules = [
         processes.ocupacion,
-        # processes.abonados_en_banco,
-        # processes.recaudacion,
-        # processes.rincon_estadisticas,
-        # processes.abonados_lpa_y_qr,
-        # processes.informes_filtrados,
-        # processes.abonados_y_rotacion,
+        processes.abonados_en_banco,
+        processes.recaudacion,
+        processes.rincon_estadisticas,
+        processes.abonados_lpa_y_qr,
+        processes.informes_filtrados,
+        processes.abonados_y_rotacion,
     ]
     all_events = []
     for module in modules:
         feeds = module.main()
         all_events.extend(feeds)
 
-    # insert_events(processes.ratios.main(all_events))
+    insert_events(processes.ratios.main(all_events))
     return "ETL ejecutado correctamente\n", 200
 
-if __name__ == "__main__":
-    entry_point(None)
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def run():
+    return entry_point(request)
