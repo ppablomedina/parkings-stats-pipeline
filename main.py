2884e8eee1109170fa89b5e1d843ddcd74b5a438
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-11-12 14:27:54 +0000

    Reorder process modules in entry_point function

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from gcp.utils import insert_events
 # --- tu entry_point original ---
 def entry_point(req):
     modules = [
-        processes.ocupacion,
-        processes.abonados_en_banco,
-        processes.recaudacion,
-        processes.rincon_estadisticas,
-        # processes.abonados_lpa_y_qr,
-        # processes.informes_filtrados,
-        # processes.abonados_y_rotacion,
+        # processes.ocupacion,
+        # processes.abonados_en_banco,
+        # processes.recaudacion,
+        # processes.rincon_estadisticas,
+        processes.abonados_lpa_y_qr,
+        processes.informes_filtrados,
+        processes.abonados_y_rotacion,
     ]
     all_events = []
     for module in modules:
